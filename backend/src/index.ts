07e3573612800c63c6365ba1bd70f245b6e8714c
Vladimir5863 <vlada.nikolic2002@gmail.com>
2026-07-04 14:01:25 +0200

    initial commit

---
+++ backend/src/index.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import { ENV } from "./config/env";
+import { clerkMiddleware } from "@clerk/express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors({ origin: ENV.FRONTEND_URL }));
+app.use(clerkMiddleware());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.json({
+    message:
+      "Welcome to Productivuty API - Powered by PostGreSQL, Drizzle ORM & Clerk Auth",
+    enpoint: {
+      users: "/api/users",
+      products: "/api/products",
+      comments: "/api/comments",
+    },
+  });
+});
+
+app.listen(ENV.PORT, () =>
+  console.log("Server is up and running on PORT:", ENV.PORT),
+);
